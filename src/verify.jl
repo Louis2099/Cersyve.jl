558ff401c58976d536cd74df4230d3fb542980a3
Louis2099 <leejiaxing180@gmail.com>
2025-02-17 19:42:23 -0500

    hold: 4D unicycle

--- src/verify.jl
+++ src/verify.jl
@@ -15,7 +15,7 @@ function verify_value(
     
     #TODO: Double posi-bound
     if tol != 0.0
-        rec_tor = ceil(Int, 1/tol)        
+        rec_tor = ceil(Int, 1/tol)      
         Y = Complement(HPolyhedron([rec_tor 0; 0 -rec_tor], [-1, -1]))
     end
 
